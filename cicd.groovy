dd51ffcd60ad7c81dcfe37ea1bb3adf454a3a2e1
Igor Todorovski <39890068+IgorTodorovskiIBM@users.noreply.github.com>
2023-06-01 09:21:37 -0400

    Update cicd.groovy to check for changes from upstream zoslib

--- cicd.groovy
+++ cicd.groovy
@@ -8,6 +8,13 @@ node('linux')
                         doGenerateSubmoduleConfigurations: false,
                         extensions: [],
                         userRemoteConfigs: [[url: "https://github.com/ZOSOpenTools/zoslibport.git"]]])
+                 // Poll changes from upstream
+                 checkout([
+                        $class: 'GitSCM',
+                        branches: [[name: '*/zopen']],
+                        doGenerateSubmoduleConfigurations: false,
+                        extensions: [],
+                        userRemoteConfigs: [[url: "https://github.com/ibmruntimes/zoslib.git"]]])
         }
   stage('Build') {
     build job: 'Port-Pipeline', parameters: [string(name: 'PORT_GITHUB_REPO', value: 'https://github.com/ZOSOpenTools/zoslibport.git'), string(name: 'PORT_DESCRIPTION', value: 'ZOSLIB is a z/OS C/C++ library. It is an extended implementation of the z/OS LE C Runtime Library.' )]
