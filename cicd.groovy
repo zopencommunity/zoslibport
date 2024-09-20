43efff3c3470a6ca4d5b311d7736ae82f20931d3
Igor Todorovski <itodorov@ca.ibm.com>
2024-09-20 17:20:56 -0400

    Change organization to zopencommunity

--- cicd.groovy
+++ cicd.groovy
@@ -7,7 +7,7 @@ node('linux')
                         branches: [[name: '*/main']],
                         doGenerateSubmoduleConfigurations: false,
                         extensions: [],
-                        userRemoteConfigs: [[url: "https://github.com/ZOSOpenTools/zoslibport.git"]]])
+                        userRemoteConfigs: [[url: "https://github.com/zopencommunity/zoslibport.git"]]])
                  // Poll changes from upstream
                  checkout([
                         $class: 'GitSCM',
@@ -17,6 +17,6 @@ node('linux')
                         userRemoteConfigs: [[url: "https://github.com/ibmruntimes/zoslib.git"]]])
         }
   stage('Build') {
-    build job: 'Port-Pipeline', parameters: [string(name: 'PORT_GITHUB_REPO', value: 'https://github.com/ZOSOpenTools/zoslibport.git'), string(name: 'PORT_DESCRIPTION', value: 'ZOSLIB is a z/OS C/C++ library. It is an extended implementation of the z/OS LE C Runtime Library.' )]
+    build job: 'Port-Pipeline', parameters: [string(name: 'PORT_GITHUB_REPO', value: 'https://github.com/zopencommunity/zoslibport.git'), string(name: 'PORT_DESCRIPTION', value: 'ZOSLIB is a z/OS C/C++ library. It is an extended implementation of the z/OS LE C Runtime Library.' )]
   }
 }
