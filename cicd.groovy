8dd6240ba9d75c24764e3f086d7323d785f5bab8
Sachin T <sachintu47@gmail.com>
2026-05-06 12:27:29 +0530

    Remove ZOPEN_DEV_BRANCH to allow test build from source

--- cicd.groovy
+++ cicd.groovy
@@ -14,7 +14,7 @@ node('linux')
                         branches: [[name: '*/zopen2']],
                         doGenerateSubmoduleConfigurations: false,
                         extensions: [],
-                        userRemoteConfigs: [[url: "https://github.com/ibmruntimes/zoslib.git"]]])
+                        userRemoteConfigs: [[url: "https://github.com/zopencommunity/zoslib.git"]]])
         }
   stage('Build') {
     build job: 'Port-Pipeline', parameters: [string(name: 'PORT_GITHUB_REPO', value: 'https://github.com/zopencommunity/zoslibport.git'), string(name: 'PORT_DESCRIPTION', value: 'ZOSLIB is a z/OS C/C++ library. It is an extended implementation of the z/OS LE C Runtime Library.' )]
