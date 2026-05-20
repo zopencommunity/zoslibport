ef7b4074d7d4ead343e619f6705e060417449b34
sachintu47 <sachintu47@gmail.com>
2026-05-19 21:48:35 -0400

    Remove upstream polling from cicd

--- cicd.groovy
+++ cicd.groovy
@@ -8,13 +8,6 @@ node('linux')
                         doGenerateSubmoduleConfigurations: false,
                         extensions: [],
                         userRemoteConfigs: [[url: "https://github.com/zopencommunity/zoslibport.git"]]])
-                 // Poll changes from upstream
-                 checkout([
-                        $class: 'GitSCM',
-                        branches: [[name: '*/zopen2']],
-                        doGenerateSubmoduleConfigurations: false,
-                        extensions: [],
-                        userRemoteConfigs: [[url: "https://github.com/zopencommunity/zoslib.git"]]])
         }
   stage('Build') {
     build job: 'Port-Pipeline', parameters: [string(name: 'PORT_GITHUB_REPO', value: 'https://github.com/zopencommunity/zoslibport.git'), string(name: 'PORT_DESCRIPTION', value: 'ZOSLIB is a z/OS C/C++ library. It is an extended implementation of the z/OS LE C Runtime Library.' )]
